20dc6f0e8547d96f57f01c7480752f63dfd5659e
cr0-git <cr0extraemail@gmail.com>
2021-08-05 19:26:33 +0100

    add function to read txt file of player names

---
+++ R/readplayers.R
@@ -0,0 +1,16 @@
+#' Turn text file into list
+#'
+#' @param path Path to text file
+#' @return List of player names
+#' @export
+#' @examples
+#' team1 <- readplayers("./team.txt")
+
+readplayers <- function(path) {
+
+  playerlist <- read.table("C:/Users/carlo/Desktop/team.txt")
+  playerlist <- as.list(levels(playerlist$V1))
+
+  return(playerlist)
+
+}
